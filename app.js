7ef6c9bcd34edd91ae079463832397499b947eeb
Amy Dreise <amy@cyberbia.com.au>
2015-05-08 22:41:53 +1000

    Displaying data.

--- app.js
+++ app.js
@@ -12,5 +12,16 @@ myApp.controller("MyController", ["$scope", "$firebaseArray",
   }
 ]);
 
+myApp.controller("TeacherController", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) {
+    var ref = new Firebase("https://ygbcnjyw3s8.firebaseio-demo.com/");
+    $scope.messages = $firebaseArray(ref);
+    var students = new Firebase("https://flickering-fire-2155.firebaseio.com/students");
+    $scope.students = $firebaseArray(students);
+    var questions = new Firebase("https://flickering-fire-2155.firebaseio.com/questions");
+    $scope.questions = $firebaseArray(questions);
+  }
+]);
+
 
 
