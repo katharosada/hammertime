f285c2a0114e29374a91cbc82d5a44a8899d3420
Katie Bell <katie@katharos.id.au>
2015-05-08 23:50:48 +1000

    Teacher page is somewhat useful now.

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ myApp.controller("MyController", ["$scope", "$firebaseArray",
     $scope.students = $firebaseArray(students);
     var questions = new Firebase("https://flickering-fire-2155.firebaseio.com/questions");
     $scope.questions = $firebaseArray(questions);
+
+    $scope.current_question = null;
+
+    $scope.set_current_question = function(key) {
+      $scope.current_question = key;
+      console.log(key);
+    };
+
+    $scope.open_question = function(key) {
+      //var question = $scope.questions[key].update({'open': true});
+      var question = new Firebase("https://flickering-fire-2155.firebaseio.com/questions/" + key);
+      question.update({'open': true});
+
+      console.log($scope.questions[key]);
+      console.log('called');
+    };
+
+    $scope.get_answer = function(student_id) {
+      console.log("student id: " + student_id);
+      var answers = $scope.questions[$scope.current_question].answers;
+      for (var key in answers) {
+        if (answers.hasOwnProperty(key)) {
+          console.log(answers[key]);
+          if (answers[key].student_id === student_id) {
+            return answers[key].answer;
+          }
+        }
+      }
+      return "No answer";
+    };
   }
 ]);
 
