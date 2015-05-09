ad8b56b13346ea08a658897fc9e1d5a3acae763b
Katie Bell <katie@katharos.id.au>
2015-05-09 12:25:43 +1000

    Experiment.

--- teacher.js
+++ teacher.js
@@ -1,11 +1,16 @@
-myApp.controller("TeacherController", ["$scope", "$firebaseArray",
-  function($scope, $firebaseArray) {
+myApp.controller("TeacherController", ["$scope", "$firebaseObject", "$firebaseArray",
+  function($scope, $firebaseObject, $firebaseArray) {
     var ref = new Firebase("https://ygbcnjyw3s8.firebaseio-demo.com/");
     $scope.messages = $firebaseArray(ref);
     var students = new Firebase("https://flickering-fire-2155.firebaseio.com/students");
     $scope.students = $firebaseArray(students);
     var questions = new Firebase("https://flickering-fire-2155.firebaseio.com/questions");
-    $scope.questions = $firebaseArray(questions);
+    //$scope.questions = $firebaseArray(questions);
+
+    $scope.questions2 = $firebaseObject(questions);
+
+    $scope.questions2.$bindTo($scope, "questions");
+
     $scope.current_question = null;
 
     $scope.set_current_question = function(key) {
@@ -20,7 +25,6 @@ myApp.controller("TeacherController", ["$scope", "$firebaseArray",
 
     $scope.close_all_question = function() {
       $scope.current_question = null;
-      console.log(key);
     };
 
 
@@ -28,26 +32,18 @@ myApp.controller("TeacherController", ["$scope", "$firebaseArray",
       //var question = $scope.questions[key].update({'open': true});
       var question = new Firebase("https://flickering-fire-2155.firebaseio.com/questions/" + key);
       question.update({'open': true});
-
-      console.log($scope.questions[key]);
-      console.log('called');
     };
 
     $scope.close_question = function(key) {
       //var question = $scope.questions[key].update({'open': true});
       var question = new Firebase("https://flickering-fire-2155.firebaseio.com/questions/" + key);
       question.update({'open': false});
-
-      console.log($scope.questions[key]);
-      console.log('called');
     };
 
     $scope.get_answer = function(student_id) {
-      console.log("student id: " + student_id);
       var answers = $scope.questions[$scope.current_question].answers;
       for (var key in answers) {
         if (answers.hasOwnProperty(key)) {
-          console.log(answers[key]);
           if (answers[key].student_id == student_id) {
             return answers[key].answer;
           }
