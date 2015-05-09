148c65d4e68af20d11621d8b3626c51b9b360ac2
Katie Bell <katie@katharos.id.au>
2015-05-09 15:43:54 +1000

    Make submit on enter work for student page.

--- app.js
+++ app.js
@@ -67,3 +67,16 @@ myApp.controller("MyController", ["$scope", "$firebaseArray",'$timeout',
   }
 ]);
 
+angular.module('hammertime').directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
